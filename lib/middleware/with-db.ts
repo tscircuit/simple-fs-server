97bc353b0d0b81b82c4a882a130dc0182ccec28a
seveibar <seveibar@gmail.com>
2024-12-14 00:10:56 -0800

    fix withdb to persist

--- lib/middleware/with-db.ts
+++ lib/middleware/with-db.ts
@@ -2,6 +2,10 @@ import type { DbClient } from "lib/db/db-client"
 import { createDatabase } from "lib/db/db-client"
 import type { Middleware } from "winterspec"
 
+declare global {
+  var db: DbClient
+}
+
 export const withDb: Middleware<
   {},
   {
@@ -9,7 +13,10 @@ export const withDb: Middleware<
   }
 > = async (req, ctx, next) => {
   if (!ctx.db) {
-    ctx.db = createDatabase()
+    if (!globalThis.db) {
+      globalThis.db = createDatabase()
+    }
+    ctx.db = globalThis.db
   }
   return next(req, ctx)
 }
